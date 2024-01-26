9848c32dc7596289f585e8b5ab336b40f35d0d0c
Just William <44903208+justxwilliam@users.noreply.github.com>
2024-01-26 21:19:51 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ulimit -n "${ULIMIT}"
+cd ${STEAMCMDDIR}
+chown "${USER}":"${GROUP}" -R /opt/
+
+if [ -e "/home/steam/.steam/sdk32/steamclient.so" ]
+then
+  echo "steamclient.so found."
+else
+  echo "steamclient.so not found."
+  su steam -c "ln -s ${STEAMCMDDIR}/linux32/steamclient.so ~/.steam/sdk32/steamclient.so"
+  if [ -e "/home/steam/.steam/sdk32/steamclient.so" ]
+  then
+    echo "steamclient.so link created."
+  fi
+fi
+
+# update
+${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType linux +login anonymous \
++force_install_dir "${SERVERDIR}/palserver/" +app_update 2394010  \
++quit
+
+# server start
+su "${USER}" -c "cd ${SERVERDIR}/palserver/"
